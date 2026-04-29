572ac29497cf8589ece0165b0df34e022a54aaf5
Arjun Febrian <arjun@Arjun-Mac.local>
2026-04-29 21:29:19 +0700

    fix idempotency logic and add playwright api test

--- main.go
+++ main.go
@@ -60,6 +60,7 @@ func createTransactionHandler(w http.ResponseWriter, r *http.Request) {
 
 			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(existingTx)
+			return
 		}
 	}
 
--- tests/api/transaction.spec.ts
+++ tests/api/transaction.spec.ts
@@ -1,4 +1,4 @@
-import { test,expect } from "@playwright/test";
+import { test,expect } from "@playwright/test"
 
 test('create transaction success', async({ request }) => {
     const res = await request.post('http://localhost:8080/transactions', {
@@ -8,8 +8,32 @@ test('create transaction success', async({ request }) => {
     })
 
     expect(res.status()).toBe(201)
-
     const body = await res.json()
-
     expect(body.status).toBe('PENDING')
 })
+
+test('same idempotency return same transaction',async({ request }) => {
+    const payload = {
+        amount: 167263,
+        idempotency_key: `idem-${Date.now()}`
+    }
+
+    const firstRes = await request.post('http://localhost:8080/transactions', {
+        data: payload
+    })
+
+    expect(firstRes.status()).toBe(201)
+
+    const firstBody = await firstRes.json()
+    const secondRes = await request.post('http://localhost:8080/transactions',{
+        data: payload
+    })
+
+    expect(secondRes.ok()).toBeTruthy()
+    const secondBody = await secondRes.json()
+
+    expect(secondBody.id).toBe(firstBody.id)
+    expect(secondBody.amount).toBe(firstBody.amount)
+    expect(secondBody.idempotency_key).toBe(firstBody.idempotency_key)
+}
+)
